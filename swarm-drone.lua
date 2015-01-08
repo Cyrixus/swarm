a6e2d0eb12bbcdb0901534f4a96a761cdc69f290
Matthew DiBernardo <cryptex.md@gmail.com>
2015-01-08 16:32:26 -0500

    Added swarm-drone. Working my way up to the behavior engine, and basic auto-mining.

---
+++ swarm-drone.lua
@@ -0,0 +1,60 @@
+--[[ swarm-drone.lua
+	Core drone behavior.
+]]--
+
+-- SwarmDrone Class Definition
+local function SwarmDrone()
+	local self = {} -- 'this' reference
+
+	-- Tick Info
+	local self.startTickSecondTime
+	local self.ticks = 0
+	local self.ticksPerSecond = 0
+
+	function init()
+		--[[ TODO
+			Step 1: Load potential behaviors (and thier constraints)
+				from disk, hold in local memory.
+			Step 2: Collect data about self and immediate surroundings
+			Step 3: Announce self to swarm-net, if possible
+		]]--
+	end
+
+	function tick()
+		-- Recalculate ticksPerSecond
+		local currentTime = os.time() -- Gets the minecraft time
+		if self.startTickSecondTime ~= nil then
+			-- If at least a second has elapsed, calculate average t/sec
+			local elapsed = currentTime - self.startTickSecondTime
+			if elapsed > 1.0 then
+				self.ticksPerSecond = self.ticks / elapsed
+				self.ticks = 0
+				self.startTickSecondTime = currentTime
+			
+			-- Avoid some possible issues with time rolling over at
+			-- midnight by reseting the clock
+			elseif elapsed < 0.0 then
+				self.startTickSecondTime = currentTime
+			end
+		else
+			-- Otherwise, start the clock
+			self.startTickSecondTime = currentTime
+		end
+
+		-- DO TICK BEHAVIOR
+		onTick() -- This may not be called EVERY tick(), in the future
+
+		-- Increment Tick Counter
+		self.ticks += 1
+	end -- EOF tick()
+
+	local function onTick()
+		--[[ FIXME: This is where the magic should start happening.
+			Step 1: Regenerate constraints (fuel state, inventory fullness)
+			Step 2: Poll OS events, handle anything relevent
+			Step 3: Determine Active Behavior via priority, queue, fuzzy logic, etc.
+			Step 4: Execute Active Behavior
+		]]--
+	end
+
+end -- EOF SwarmDrone Definition
