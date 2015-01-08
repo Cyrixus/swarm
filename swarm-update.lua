dc3a2ddf30ca68251d9be62ee4978bba95a2beb9
Matthew DiBernardo <cryptex.md@gmail.com>
2015-01-08 15:25:29 -0500

    swarm-update now with better memory control.

--- swarm-update.lua
+++ swarm-update.lua
@@ -3,28 +3,28 @@
 ]]--
 
 -- Default host definitions
-swarmHost = "http://raw.githubusercontent.com/Cyrixus/swarm/master/"
-swarmFiles = swarmHost .. "swarm-files.cfg"
+local swarmHost = "http://raw.githubusercontent.com/Cyrixus/swarm/master/"
+local swarmFiles = swarmHost .. "swarm-files.cfg"
 
 if http then
     print("Beginning update...")
     -- We can use HTTP, get the latest set of swarm files
-    fileList = http.get(swarmFiles)
+    local fileList = http.get(swarmFiles)
     if fileList == nil then
         error("Could not connect to swarmHost [" .. swarmFiles .. "]")
     else
         -- Got the file list successfully, so now read each file and save it to disk
         print("File list retrieved, downloading swarm...")
-        fileName = fileList.readLine()
+        local fileName = fileList.readLine()
         while fileName ~= nil do
             -- Retrieve the file from swarmHost
-            file = http.get(swarmHost .. fileName)
+            local file = http.get(swarmHost .. fileName)
             if file == nil then
                 error("Could not fetch file [" .. fileName .. "]")
             end
             
             -- Write the file to disk
-            outFile = fs.open(shell.resolve("") .. "/" .. fileName, "w")
+            local outFile = fs.open(shell.resolve("") .. "/" .. fileName, "w")
             outFile.write(file.readAll())
             outFile.close()
             print("Saved " .. fileName .. "...")
