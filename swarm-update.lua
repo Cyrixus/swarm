90fb872c8bc0bb374c359503a2a795c1a6590dcb
Matthew DiBernardo <cryptex.md@gmail.com>
2015-01-08 13:57:42 -0500

    Added more verbose output, fixed some obvious bugs and oversights.

--- swarm-update.lua
+++ swarm-update.lua
@@ -1,23 +1,38 @@
--- swarm-update.lua
--- Fetches the latest version of swarm from github, then updates all swarm files.
+--[[ swarm-update.lua
+    Fetches the latest version of swarm from github, then updates all swarm files.
+]]--
+
+-- Default host definitions
 swarmHost = "http://raw.githubusercontent.com/Cyrixus/swarm/master/"
 swarmFiles = swarmHost .. "swarm-files.cfg"
 
 if http then
+    print("Beginning update...")
     -- We can use HTTP, get the latest set of swarm files
     fileList = http.get(swarmFiles)
-    if file == nil then
+    if fileList == nil then
         error("Could not connect to swarmHost [" .. swarmFiles .. "]")
     else
         -- Got the file list successfully, so now read each file and save it to disk
+        print("File list retrieved, downloading swarm...")
         fileName = fileList.readLine()
         while fileName ~= nil do
+            -- Retrieve the file from swarmHost
             file = http.get(swarmHost .. fileName)
+            if file == nil then
+                error("Could not fetch file [" .. fileName .. "]")
+            end
             
+            -- Write the file to disk
             outFile = fs.open(shell.resolve("") .. "/" .. fileName, "w")
             outFile.write(file.readAll())
             outFile.close()
+            print("Saved " .. fileName .. "...")
+
+            -- Get the next file in the list
+            fileName = fileList.readLine()
         end
+        print("Done.")
     end
     
 else
