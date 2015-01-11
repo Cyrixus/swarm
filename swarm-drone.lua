827e7beeb1e268ff596038fcfcb9d62a04fb0136
Matt <cryptex.md@gmail.com>
2015-01-11 01:20:25 -0500

    Location system and GOTO verb implemented.

--- swarm-drone.lua
+++ swarm-drone.lua
@@ -13,7 +13,7 @@ local resourceDir = "/resources/"
 local conditionalDir = "/conditionals/"
 -- Directories for information specific to this drone.
 local identityDir = "/id/" -- As in the psychological id. This dir contains the knowledge of "self".
-
+local locationsDir = "/id/locs"
 
 
 -- Function for loading libs
@@ -24,6 +24,34 @@ local function loadLib(libLocation)
 	end
 	return true
 end
+
+-- A simple UUID method borrowed from https://gist.github.com/jrus/3197011 and simplified further
+-- because we don't REALLY need a whole UUID for anything here
+local random = math.random
+function UUID()
+    local template ='xxxxxxxx'
+    return string.gsub(template, '[xy]', function (c)
+        local v = (c == 'x') and random(0, 0xf) or random(8, 0xb)
+        return string.format('%x', v)
+    end
+end
+
+
+--[[ Load Libraries ]]--
+-- Load the JSON encoding/decoding library
+-- (Big thanks to Jeffrey Friedl for his library! See notice in lib/JSON)
+local apiLocation = shell.resolve("") .. libDir .. "JSON"
+if not os.loadAPI(apiLocation) then error("Failed to load JSON API @ [" .. apiLocation .. "], aborting.") end
+local JSON = JSON.OBJDEF:new() -- Because, you know, CC just letting us load libs normally was too hard.
+
+-- Load the mobility turtle API
+apiLocation = shell.resolve("") .. libDir .. "mobility"
+if not os.loadAPI(apiLocation) then error("Failed to load mobility API @ [" .. apiLocation .. "], aborting." end)
+
+-- Load the IDLE behavior, because we're going to be running it frequently
+local idleLoc = shell.resolve("") .. verbDir .. "IDLE"
+if not os.loadAPI(idleLoc) then error("Failed to load IDLE behavior @ [" .. idleLoc .. "], aborting.") end
+
 
 --[[ Engine Calls ]]--
 function checkConditional(conditional, params)
@@ -64,35 +92,75 @@ function matchResourceByName(resource, name)
 	return false
 end
 
--- Load the JSON encoding/decoding library
--- (Big thanks to Jeffrey Friedl for his library! See notice in lib/JSON)
-local apiLocation = shell.resolve("") .. libDir .. "JSON"
-if not os.loadAPI(apiLocation) then error("Failed to load JSON API @ [" .. apiLocation .. "], aborting.") end
-local JSON = JSON.OBJDEF:new() -- Because, you know, CC just letting us load libs normally was too hard.
+function forEachLocation(callback, locID) -- Make sure callback is a function accepting a table as a param
+	local locsFolder = shell.resolve("") .. locationsDir
+	local locs = fs.list(locsFolder)
+	for i, locFile in ipairs(locs) do
+		-- Check to see if this location is in the set we're looking for
+		if locID == nil or string.find(locFile, locID, 1, true) then
+			local locFileName = shell.resolve("") .. locationsDir .. locFile
+		
+			-- Open each non-dir file
+			if not fs.isDir(locFileName) then
+				local f = fs.open(locFileName, "r")
+				if not f then
+					error("Couldn't open file [" .. locFileName .. "]")
+				end
+				
+				-- Decode its contents into a lua table
+				local loc = JSON:decode(f.readAll())
+				f.close()
+				
+				-- Call the callback
+				callback(loc)
+			end
+		end
+	end
+end
 
--- Load the mobility turtle API
-apiLocation = shell.resolve("") .. libDir .. "mobility"
-if not os.loadAPI(apiLocation) then error("Failed to load mobility API @ [" .. apiLocation .. "], aborting." end)
-
--- Load the IDLE behavior, because we're going to be running it frequently
-local idleLoc = shell.resolve("") .. verbDir .. "IDLE"
-if not os.loadAPI(idleLoc) then error("Failed to load IDLE behavior @ [" .. idleLoc .. "], aborting.") end
+function createPointLocationXZ(locID, x, z, height, facing, uuid)
+	local locsFolder = shell.resolve("") .. locationsDir
+	
+	-- Validate all of the params, or replace the missing bits with parts of our current location
+	local currentPos = mobility.getPosition()
+	local currentFacing = mobility.getFacing()
+	x = x or currentPos.x
+	z = z or currentPos.z
+	height = height or currentPos.y
+	facing = facing or currentFacing
+	uuid = uuid or UUID()
+	
+	-- Build the location table
+	local loc = { "name" = locID, "uuid" = uuid, "x" = x, "y" = height, "z" = z, "facing" = facing }
+	
+	-- Write the location to a file
+	local f = fs.open(locsFolder .. loc.name .. "-" .. loc.uuid, "w")
+	if not f then
+		error("Couldn't loc file for writing!")
+	end
+	f.write(JSON:encode(loc))
+	f.close()	
+end
 
 
 --[[ SwarmDrone Class Definition ]]--
 local function SwarmDrone()
 	local self = {} -- 'this' reference
+	
+	-- UUID
+	local self.uuid = UUID()
+	function self.uuid() return self.uuid end
 
 	-- Tick Info
-	self.startTickSecondTime = nil
-	self.ticks = 0
-	self.ticksPerSecond = 0
+	local self.startTickSecondTime = nil
+	local self.ticks = 0
+	local self.ticksPerSecond = 0
 	
 	-- Idle Tick Info
-	self.lastIdleTime = 0
+	local self.lastIdleTime = 0
 	
 	-- Behaviors
-	self.behaviors = {}
+	local self.behaviors = {}
 
 	--[[ Initialization ]]--
 	function self.init()
@@ -130,20 +198,108 @@ local function SwarmDrone()
 			
 		print("Successfully loaded [" .. #self.behaviors .. "] behaviors from disk...")
 		
-		-- TODO: Collect data about self and immediate surroundings (needs RESOURCE definitions)
-		--[[ TURTLE-ONLY INIT ]]--
-		local isTurtle = checkConditional("IS_TURTLE")
-		if isTurtle then
-			print("Turtle detected! Performing turtle-only initialization...")
-			executeVerb("CHECK_SURROUNDINGS")
+		
+		-- TODO: Collect data about self
+		
+		
+		-- TODO: Load user-created id files(?)
+		
+		
+		--[[ Check for/create important id files ]]--
+		-- Fetch uuid, if it exists
+		local uuidFile = shell.resolve("") .. identityDir .. "uuid"
+		if fs.exists(uuidFile) and not fs.isDir(uuidFile) then
+			local f = fs.open(uuidFile, "r")
+			if not f then
+				error("uuid file exists, but couldn't be opened!")
+			end
+			local uuid = f.readLine()
+			f.close()
+			if string.len(uuid) == 8 then
+				self.uuid = uuid
+			else
+				print("WARNING: Loaded uuid was invalid, replacing.")
+			end
 		end
 		
+		-- Set this machine's label to the current uuid
+		-- This allows files to be saved even when the computer is broken.
+		os.setComputerLabel("swarm-drone-"..self.uuid)
+		
+		-- Write the uuid to disk for later
+		local f = fs.open(uuidFile, "w")
+		if not f then
+			error("Couldn't uuid file for writing!")
+		end
+		f.write(self.uuid)
+		f.close()
+		
+		
 		-- TODO: Announce self to swarm-net
+		
+		
+		-- Attempt to determine absolute position
+		local x, y, z = gps.locate(5)
+		if not x then
+			print("WARNING: Could not establish location via GPS. Please manually override the "
+				.. "generated LASTPOS-XXXX file, providing the absolute coordinates of this machine.")
+		else
+			mobility.setPositionXZ(x, z, y)
+		end
+		
+		
+		--[[ TURTLE-ONLY INIT ]]--
+		local isTurtle = checkConditional("IS_TURTLE")
+		if not isTurtle then
+			return
+		end
+		
+		-- Check Immediate Surroundings
+		print("Turtle detected! Performing turtle-only initialization...")
+		executeVerb("CHECK_SURROUNDINGS")
+		
+		-- Save the starting position with the identifier HOME, unless one already exists
+		local doesHomeExist = false
+		forEachLocation(function() doesHomeExist = true end, "HOME")
+		
+		if not doesHomeExist then
+			createPointLocationXZ("HOME")
+		end
+		
+		-- Attempt to determine facing
+		local oldPos = mobility.getPosition()
+		if x and mobility.moveForward(3) then -- Check that our previous GPS attempt was good, and that we have some space
+			-- Get a new reading and figure out our facing
+			x, y, z = gps.locate(5)
+			if x then
+				local facingVec = vector.new(x,y,z) - oldPos
+				
+				-- North
+				if facingVec.z < 0 then
+					mobility.setFacing("north")
+				end
+				
+				-- South
+				if facingVec.z > 0 then
+					mobility.setFacing("south")
+				end
+				
+				-- East
+				if facingVec.x > 0 then
+					mobility.setFacing("east")
+				end
+				
+				-- West
+				if facingVec.x < 0 then
+					mobility.setFacing("west")
+				end
+			end
+		end
 	end
 
 	--[[ doIDLE ]]--
 	local function doIDLE()
-		IDLE.execute()
+		IDLE.execute(self)
 		self.lastIdleTime = os.clock()
 	end
 
